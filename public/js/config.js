487e8cb87085be6b45e48c0745b4c1ade0e99f45
mszypke <mszypke@bluesoft.com>
2023-12-11 10:09:08 +0100

    Updated user pool

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_PPkynf3wh', // e.g. eu-east-2_uXboG5pAb
-        userPoolClientId: '16kjssnfp8bm7v6ugksic72rl0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_Qh48UIZ1J', // e.g. eu-east-2_uXboG5pAb
+        userPoolClientId: '15hhvh7cnct9imuatln22p82hr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
